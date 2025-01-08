be3d72fea412c752b359d7a737c98b8d97496ae2
aswinbabs <aswinbabzzzz@gmail.com>
2025-01-08 14:26:02 +0530

    completed Assignment

--- Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2.cpp
@@ -1,20 +1,165 @@
-// Assignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm>
 
-#include <iostream>
+//function to create a new account
 
-int main()
+
+class BankAccounts {
+private:
+	static int counter;
+	int sl_no;
+	std::string accname;
+	int accnum;
+	double accbalance;
+	// Access specifier
+public:
+	// Member Functions()
+
+	BankAccounts(std::string name, int num, double balance)
+	{
+		sl_no = ++counter;
+		accname = name;
+		accnum = num;
+		accbalance = balance;
+	}
+	
+	 
+	
+	 
+	//function to display
+	void displayDetails()
+	{ 
+		std::cout << "\nSl.No:" 
+			      << sl_no << "\nAccount Holder's name:" 
+			      << accname << "\nAccount Number:" 
+			      << accnum << "\nAccount Balance:"
+			      << accbalance; 
+	}
+
+	//function to get balance
+	double getBalance()
+	{
+		return accbalance;
+	}
+
+	//function to get accnum
+	int getAccountNumber()
+	{
+		return accnum;
+	}
+};
+
+//initialize counter outside class
+int BankAccounts::counter = 0;
+
+//function to create new account
+void addBankAccount(std::vector<BankAccounts> &v, std::string& name, int num, double balance)
 {
-    std::cout << "Hello World!\n";
+	
+	v.emplace_back(name, num, balance);
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+//function to sort accounts based on balance
+void sortAccounts(std::vector<BankAccounts>&v)
+{
+   //using std::sort()
+	std::sort(v.begin(), v.end(), [](BankAccounts& a, BankAccounts& b)
+		{
+			//Ascending order
+			return a.getBalance() < b.getBalance();
+		});
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+//function to search account using account number
+void searchAccount(std::vector<BankAccounts>& v, int searchNum)
+{
+	auto it = std::find_if(v.begin(), v.end(), [searchNum](BankAccounts& num)
+		{
+			return num.getAccountNumber() == searchNum;
+		});
+
+	// If account found, display details, else show an error message
+	if (it != v.end()) {
+		std::cout << "\nAccount found:\n";
+		it->displayDetails();  // Display account details
+	}
+	else {
+		std::cout << "\nAccount not found.\n";  // Account not found
+	}
+}
+int main()
+{
+	// Vector of class objects 
+	std::vector<BankAccounts> v;
+	int choice, tempNum,sNum;
+	std::string tempName;
+	double tempBalance;
+
+
+
+	//prev data
+	v.emplace_back("Aswin", 100000000, 55000); //Name ,  A/c number , Balance
+	v.emplace_back("Aquinas", 100000001, 30000);
+	
+	
+
+	while (1)
+	{
+
+	//Selects b/w choice
+	std::cout << "\n1. Add new bank account\n";
+	std::cout << "2. Display all the account details\n";
+	std::cout << "3. Sort by balance\n";
+	std::cout << "4. Search by account number\n";
+	std::cin >> choice;
+	
+		switch (choice)
+		{
+		case 1:
+
+			//new account
+			std::cout << "\nFor new registration Enter Name,A/C number and Balance\n";
+			std::cin >> tempName >> tempNum >> tempBalance;
+			addBankAccount(v, tempName, tempNum, tempBalance);
+			break;
+		case 2:
+
+			//List of all account details
+			std::cout << "\nFinal list";
+
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i].displayDetails();
+			}
+			break;
+		case 3:
+
+			//List of accounts sorted by balance
+			sortAccounts(v);
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i].displayDetails();
+			}
+			break;
+		case 4:
+
+			//Search using account number
+			std::cout << "\nEnter the Account Number to search: ";
+			std::cin >> sNum;
+			searchAccount(v, sNum);
+			break;
+		default:
+
+			//
+			std::cout << "\nInvalid choice, please try again.\n";
+			break;
+		}
+	}
+
+	return 0;
+
+}
