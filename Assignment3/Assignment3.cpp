20577396ac15bd7cb4cab9e0b47597dbd5f3c099
aswinbabs <aswinbabzzzz@gmail.com>
2025-01-08 15:32:26 +0530

    Assignment3 completed

--- Assignment3/Assignment3.cpp
+++ Assignment3/Assignment3.cpp
@@ -3,18 +3,35 @@
 
 #include <iostream>
 
+template<typename T>
+class TypeChecker
+{
+private:
+    T size;
+public:
+    int printSize() 
+    { 
+
+        return sizeof(size);
+    }
+    
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+    TypeChecker<int> a;
+    TypeChecker<bool> b;
+    TypeChecker<double> c;
+    TypeChecker<std::string> d;
+    
+    std::cout << a.printSize() << std::endl;
+    std::cout << b.printSize() << std::endl;
+    std::cout << c.printSize() << std::endl;
+    std::cout << d.printSize() << std::endl;
+
+
+     std::cin.get();
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
